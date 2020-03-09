11989e868542b026af945e6954486ee0b9399396
Ernest Bruno <ernest.bruno94@yahoo.com>
2020-03-09 10:15:26 -0700

    Add TextInput and Button. Add styles as well.

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{padding: 70}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <TextInput placeholder='Course Goal' style={{borderBottomColor: 'black', borderWidth: 1, padding: 10, width: '80%'}}/>
+        <Button title='ADD'/>
+      </View>
+      <View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
