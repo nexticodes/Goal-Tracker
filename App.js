90a01c2efbe0d6eb3aa271113e5f6fb7d6e487d2
Ernest Bruno <ernest.bruno94@yahoo.com>
2020-03-09 11:05:44 -0700

    Add styles to input, text input and button.

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{padding: 70}}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-        <TextInput placeholder='Course Goal' style={{borderBottomColor: 'black', borderWidth: 1, padding: 10, width: '80%'}}/>
-        <Button title='ADD'/>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Course Goal' style={styles.input} />
+        <Button title='ADD' />
       </View>
       <View>
       </View>
@@ -15,5 +15,18 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  
+  screen: {
+    padding: 70
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    borderBottomColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+    width: '80%'
+  }
 });
