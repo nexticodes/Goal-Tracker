d49d22716fd998fdbbe9c2ac8320b4e334698480
Ernest Bruno <ernest.bruno94@yahoo.com>
2020-03-09 11:20:38 -0700

    Attach event listener to button and allow listing of goals.

--- App.js
+++ App.js
@@ -2,13 +2,31 @@ import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const handleInput = (enteredText) => {
+    setEnteredGoal(enteredText);
+  }
+
+  const handleAddGoal = () => {
+    setCourseGoals(currentGoals => [...courseGoals, enteredGoal]);
+  }
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder='Course Goal' style={styles.input} />
-        <Button title='ADD' />
+        <TextInput 
+          placeholder='Course Goal' 
+          style={styles.input} 
+          onChangeText={handleInput}
+          value={enteredGoal}
+        />
+        <Button title='ADD' onPress={handleAddGoal} />
       </View>
       <View>
+        {courseGoals.map((e,i) => <View key={i} style={styles.listItem}><Text>{e}</Text></View>)}
       </View>
     </View>
   );
@@ -21,12 +39,20 @@ const styles = StyleSheet.create({
   inputContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center'
+    alignItems: 'center',
+    marginBottom: 10
   },
   input: {
     borderBottomColor: 'black',
     borderWidth: 1,
     padding: 10,
     width: '80%'
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
   }
 });
